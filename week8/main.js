13150828e409c733d6bc1c026017653200ba3575
murtazaHw26 <145095363+murtazaHw26@users.noreply.github.com>
2023-11-03 15:36:47 +0400

    Update main.js

--- week8/main.js
+++ week8/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://github.com/murtazaHw26/webDev/week8/cities1.json');
+ourRequest.open('GET', 'https://github.com/murtazaHw26/webDev/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 console.log(ourData[0]);
