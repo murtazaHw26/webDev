e0c13d9a34030161e776ae29d892a7ee26ba2a7a
murtazaHw26 <145095363+murtazaHw26@users.noreply.github.com>
2023-11-07 10:37:27 +0400

    testing ui

--- week8/main.js
+++ week8/main.js
@@ -1,7 +1,15 @@
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://murtazahw26.github.io/webDev/cities1.json');
+ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
+renderHTML(ourData);
 };
 ourRequest.send();
+});
+function renderHTML(data){
+var htmlString = "this is a test";
+cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
