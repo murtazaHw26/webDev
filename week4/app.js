5e6f701a04cb91fb8c0aec31b79bc9db81e0c7b3
murtazaHw26 <145095363+murtazaHw26@users.noreply.github.com>
2023-11-07 12:28:34 +0400

    completed

--- week4/app.js
+++ week4/app.js
@@ -14,9 +14,32 @@ btn.addEventListener('click', function(){
     }else {
         ourRequest.open(`GET`, `http://api.openweathermap.org/data/2.5/forecast?q=${formattedText}&appid=` + apiKey);
         ourRequest.onload = function(){
-            var ourData = JSON.parse(ourRequest.responseText)
-            console.log(ourData['city'])
+            if (ourRequest.status >= 200 && ourRequest.status < 400) {
+                var ourData = JSON.parse(ourRequest.responseText);
+                console.log(ourData)
+                var description = ourData['list'][0]['weather'][0]['description']
+                var temperature = parseFloat((ourData['list'][0]['main']['temp'] - 273.15).toFixed(2))
+                var wind = ourData['list'][0]['wind']['speed']
+                renderHTML(formattedText, description, temperature, wind); 
+            } else {
+                if(ourRequest.status == 404) {
+                    alert("This city does not exist..")
+                } else {
+                console.error('Error fetching data:', ourRequest.status);
+            }
+                
+            }
+        }
+        ourRequest.onerror = function() {
+            console.error('Network error occurred');
         }
         ourRequest.send();
     }
 })
+function renderHTML(city, description, temperature, wind){
+    var htmlString = "";
+    htmlString += "<p>" + "The weather in " + city + ' is ' + description + ".</p>";
+    htmlString += "<p>" + "The temperature is " + temperature + '°C' + " with a wind speed of " + wind + "m/s." + "</p>";
+    htmlString += "<hr>";
+    info.insertAdjacentHTML('beforeend' , htmlString);
+    }
